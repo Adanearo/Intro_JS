0d06d622f7ffdb969651982ada37d3c144fd1564
Adanearo <aramosl1701@alumno.ipn.mx>
2022-05-31 19:52:14 -0500

    Create Actividad_1.js

---
+++ Actividad_1.js
@@ -0,0 +1,26 @@
+//Problema 1
+var numero=prompt("Dame un numero, mortal")
+numero
+var multi=numero*2
+console.log(multi)
+
+//Problema 2
+var num1=prompt("Dame un numero, mortal")
+num1
+var num2=prompt("No fue suficiente, dame otro")
+num2
+var resta=num1-num2
+console.log(resta)
+
+//Problema 3
+var radio=5.3
+const pi=3.1416
+var área=pi*(radio)**2
+console.log(área)
+
+//Problema 4
+var D=8
+var d=6
+var units="cm"
+var area=((D*d)/2)+units
+console.log(area)
